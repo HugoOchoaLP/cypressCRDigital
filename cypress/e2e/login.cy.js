04fdc33133d67a76b38974d5741146e3875f8c08
HugoOchoaLP <a00835999@tec.mx>
2025-05-04 16:47:08 -0600

    borrarComentario

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,21 +7,7 @@ describe("Login directamente en la pantalla", () => {
         cy.get("#password").type(user.password);
         cy.get(".inline-flex").click(); // Botón de login
       });
-  
-      // Hacer clic en "New Project"
-      //cy.get('button:contains("New Project")').click();
-  
-      // Llenar los campos del nuevo proyecto
-      //cy.get('#projectName').type('Mi proyecto de prueba');
-      //cy.get('#description').type('Este es un proyecto creado desde Cypress');
-  
-      // Enviar el formulario
-      //cy.get('button:contains("Create")').click();
-  
-      // Verificar que el proyecto fue creado
-      //cy.contains('Mi proyecto de prueba').should('exist');
-  
-      // Seleccionar el proyecto recién creado
+
       cy.contains("Mi proyecto de prueba");
   
     });
