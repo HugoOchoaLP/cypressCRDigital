4bcde5947ec932d2e1faf06d69e4d0836e193252
Jorge Andrés Palacios Plascencia <jorgepalaciospg@outlook.com>
2025-06-08 15:36:22 -0600

    updated link to deployed version

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://worksense.4.236.22.176.sslip.io",
+    baseUrl: "https://qa-s4-frontend-qa-sprint4.up.railway.app",
     chromeWebSecurity: false, // important for local storage / iframes
   },
 
